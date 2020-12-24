08c715d5d122ea9ab42bc601ad5a816e27e1585e
Niki Dokovski <nickytd@gmail.com>
2020-12-24 10:27:54 +0100

    update priority class

--- examples/install-es.sh
+++ examples/install-es.sh
@@ -4,6 +4,8 @@ sourcedir=$(dirname "$0")
 
 set -eo pipefail
 
+templates=$1
+
 echo "setting up kubernetes-logging"
 
 kubectl create namespace logging \
@@ -11,11 +13,13 @@ kubectl create namespace logging \
 
 rm -f $sourcedir/templates.yaml
 
-#generated templates
-helm template elk \
-   -n logging --create-namespace \
-   -f $sourcedir/k8s-logging-values.yaml $sourcedir/.. \
-   --debug > $sourcedir/templates.yaml
+if [ ! -z ${templates} ]; then
+  echo " generate helm templates"
+  helm template elk \
+     -n logging --create-namespace \
+     -f $sourcedir/k8s-logging-values.yaml $sourcedir/.. \
+     > $sourcedir/${templates}
+fi
 
 helm upgrade elk \
    -n logging --create-namespace \
