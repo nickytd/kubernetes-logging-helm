3aa2cba2807a19ed6ebe3ea81c3571a899f49ce5
Niki Dokovski <nickytd@gmail.com>
2020-12-25 18:04:59 +0100

    update cluster config

--- examples/setup-cluster.sh
+++ examples/setup-cluster.sh
@@ -14,9 +14,9 @@ then
 fi
 
 echo "create cluster"
-kind create cluster --name elk --config kind-config.yaml
+kind create cluster --name kind --config kind-config.yaml
 
-kubectl cluster-info --context kind-elk
+kubectl cluster-info --context kind
 
 echo "deploy nginx-ingress controler"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
