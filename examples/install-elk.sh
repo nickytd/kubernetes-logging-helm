fda7f4b42ca9c32caf57e898c4134f4c716851cf
Niki Dokovski <nickytd@gmail.com>
2021-01-29 15:58:13 +0100

    update examples setup script

--- examples/install-elk.sh
+++ examples/install-elk.sh
@@ -4,29 +4,44 @@ sourcedir=$(dirname "$0")
 
 set -eo pipefail
 
-templates=$1
+
 
 echo "setting up kubernetes-logging"
 
 kubectl create namespace logging \
   --dry-run=client -o yaml | kubectl apply -f -
 
-rm -f $sourcedir/templates.yaml
-
-if [ ! -z ${templates} ]; then
-  echo " generate helm templates"
-  helm template elk \
-     -n logging --create-namespace \
-     -f $sourcedir/k8s-logging-kind-values.yaml $sourcedir/.. \
-     > $sourcedir/${templates}
-fi
-
 helm upgrade elk \
    -n logging --create-namespace \
    -f $sourcedir/k8s-logging-kind-values.yaml $sourcedir/.. \
    --install
 
-
 kubectl apply -f $sourcedir/elk-ingresses.yaml \
   --dry-run=client -o yaml | kubectl apply -f -
 
+for var in "$@"
+do
+    if [[ "$var" = "--with-lb" ]]; then
+      echo "creating kibana LoadBalancer"
+      
+      kubectl expose svc/elk-kibana \
+        -n logging --name elk-kibana-lb --type=LoadBalancer \
+        --port 5601 --target-port=5601 \
+        --dry-run=client -o yaml | kubectl apply -f -
+
+      echo "creating es LoadBalancer"
+      
+      kubectl expose svc/elk-client \
+        -n logging --name elk-client-lb --type=LoadBalancer \
+        --port 9200 --target-port=9200 \
+         --dry-run=client -o yaml | kubectl apply -f -
+    fi
+
+    if [[ "$var" = "--templates" ]]; then
+      echo " generate helm templates"
+      
+      helm template elk -n logging \
+        -f $sourcedir/k8s-logging-kind-values.yaml $sourcedir/.. \
+        > $sourcedir/templates.yaml
+    fi
+done      
