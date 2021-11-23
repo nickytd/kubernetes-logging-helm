23d6988bd21b792b1f05177a08e56c8716e5dc70
Niki Dokovski <nickytd@gmail.com>
2021-11-23 13:42:12 +0100

    update in-cluster certificates

--- charts/certificates/setup.sh
+++ charts/certificates/setup.sh
@@ -39,19 +39,19 @@ openssl x509 -subject -nameopt RFC2253 -noout -in $BASEDIR/transport/node.pem
 openssl pkcs12 -export -name node -out $BASEDIR/transport/node.p12 -inkey $BASEDIR/transport/node-key.pem -passout "pass:92cdf688aac64d17b230" -in $BASEDIR/transport/node.pem -CAfile $BASEDIR/ca/root-ca/root-ca.pem
 
 # efk Rest cert
-openssl genrsa -out $BASEDIR/efk-rest/efk-rest-key-temp.pem 2048 
+openssl genrsa -out $BASEDIR/ofd-rest/ofd-rest-key-temp.pem 2048 
 
-openssl pkcs8 -inform PEM -outform PEM -in $BASEDIR/efk-rest/efk-rest-key-temp.pem \
-  -topk8 -nocrypt -v1 PBE-SHA1-3DES -out $BASEDIR/efk-rest/efk-rest-key.pem
-openssl req -new -key $BASEDIR/efk-rest/efk-rest-key.pem -out $BASEDIR/efk-rest/efk-rest.csr \
-  -batch -verbose -config $BASEDIR/efk-rest/efk-rest.conf -extensions req_ext
-openssl x509 -req -days 365 -in $BASEDIR/efk-rest/efk-rest.csr -CA $BASEDIR/ca/root-ca/root-ca.pem \
-  -CAkey $BASEDIR/ca/root-ca/root-ca-key.pem -out $BASEDIR/efk-rest/efk-rest.pem \
-  -extensions req_ext -extfile $BASEDIR/efk-rest/efk-rest.conf
-openssl x509 -subject -nameopt RFC2253 -noout -in $BASEDIR/efk-rest/efk-rest.pem
-openssl pkcs12 -export -name efk-rest -out $BASEDIR/efk-rest/efk-rest.p12 \
-  -inkey $BASEDIR/efk-rest/efk-rest-key.pem -passout "pass:92cdf688aac64d17b230" \
-  -in $BASEDIR/efk-rest/efk-rest.pem -CAfile $BASEDIR/ca/root-ca/root-ca-key.pem
+openssl pkcs8 -inform PEM -outform PEM -in $BASEDIR/ofd-rest/ofd-rest-key-temp.pem \
+  -topk8 -nocrypt -v1 PBE-SHA1-3DES -out $BASEDIR/ofd-rest/ofd-rest-key.pem
+openssl req -new -key $BASEDIR/ofd-rest/ofd-rest-key.pem -out $BASEDIR/ofd-rest/ofd-rest.csr \
+  -batch -verbose -config $BASEDIR/ofd-rest/ofd-rest.conf -extensions req_ext
+openssl x509 -req -days 365 -in $BASEDIR/ofd-rest/ofd-rest.csr -CA $BASEDIR/ca/root-ca/root-ca.pem \
+  -CAkey $BASEDIR/ca/root-ca/root-ca-key.pem -out $BASEDIR/ofd-rest/ofd-rest.pem \
+  -extensions req_ext -extfile $BASEDIR/ofd-rest/ofd-rest.conf
+openssl x509 -subject -nameopt RFC2253 -noout -in $BASEDIR/ofd-rest/ofd-rest.pem
+openssl pkcs12 -export -name ofd-rest -out $BASEDIR/ofd-rest/ofd-rest.p12 \
+  -inkey $BASEDIR/ofd-rest/ofd-rest-key.pem -passout "pass:92cdf688aac64d17b230" \
+  -in $BASEDIR/ofd-rest/ofd-rest.pem -CAfile $BASEDIR/ca/root-ca/root-ca-key.pem
 
 
 # opensearch truststore (to connect to IdP)
@@ -64,7 +64,7 @@ rm -f $BASEDIR/ca/http.truststore
 
 keytool -import -file $BASEDIR/ca/root-ca/root-ca.pem -alias RootCA -keystore $BASEDIR/ca/http.truststore -storepass 92cdf688aac64d17b230 -noprompt
 
-keytool -importkeystore -alias efk-rest -deststorepass 92cdf688aac64d17b230 -destkeystore keystore.jks -srckeystore $BASEDIR/efk-rest/efk-rest.p12 -srcstorepass 92cdf688aac64d17b230 -noprompt
+keytool -importkeystore -alias ofd-rest -deststorepass 92cdf688aac64d17b230 -destkeystore keystore.jks -srckeystore $BASEDIR/ofd-rest/ofd-rest.p12 -srcstorepass 92cdf688aac64d17b230 -noprompt
 keytool -importkeystore -alias admin -deststorepass 92cdf688aac64d17b230 -destkeystore keystore.jks -srckeystore $BASEDIR/admin/admin.p12 -srcstorepass 92cdf688aac64d17b230 -noprompt
 keytool -importkeystore -alias node -deststorepass 92cdf688aac64d17b230 -destkeystore keystore.jks -srckeystore $BASEDIR/transport/node.p12 -srcstorepass 92cdf688aac64d17b230 -noprompt
 
