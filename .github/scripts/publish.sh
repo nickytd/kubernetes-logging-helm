fcdbb801b116bc20a35a1314464d9c268756bf37
Niki Dokovski <nickytd@gmail.com>
2022-03-02 18:54:25 +0100

    test release process

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -19,5 +19,5 @@ echo "git_base_url 	: $git_base_url"
 echo "git_upload_url: $git_upload_url"
 
 $tmp/cr package chart
-$tmp/cr upload -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url -c "$(git rev-parse HEAD)" --skip-existing
-$tmp/cr index -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url -c "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --push
+$tmp/cr upload -o $owner -r $git_repo -t $token -b $git_base_url -u $git_upload_url -c "$(git rev-parse HEAD)" --skip-existing
+$tmp/cr index -o $owner -r $git_repo -t $token -b $git_base_url -u $git_upload_url -c "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --push
