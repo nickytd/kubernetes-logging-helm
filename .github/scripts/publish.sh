f1767b2fcd1a267e17da1114a47f0db2dcb212d5
Niki Dokovski <nickytd@gmail.com>
2022-03-02 17:22:13 +0100

    switch to shell action

---
+++ .github/scripts/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version="v1.3.0"
+owner=$(cut -d '/' -f 1 <<< "$GITHUB_REPOSITORY")
+git_repo=$(cut -d '/' -f 2 <<< "$GITHUB_REPOSITORY")
+git_base_url="https://api.github.com/"
+git_upload_url="https://uploads.github.com/"
+
+
+echo "Installing chart-releaser... $version"
+tmp=`mktemp -d`
+curl -sSLo $tmp/cr.tar.gz  "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
+tar -xzf $tmp/cr.tar.gz -c
+rm -f $tmp/cr.tar.gz
+
+$tmp/cr package chart
+$tmp/cr upload -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url --skip-existing
+$tmp/cr index -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url
