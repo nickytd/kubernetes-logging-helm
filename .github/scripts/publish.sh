e910ebef5b93bb77e38cea00e5229f0096a43228
Niki Dokovski <nickytd@gmail.com>
2022-03-02 18:52:54 +0100

    test release process

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -13,6 +13,11 @@ curl -Lo $tmp/cr.tar.gz  "https://github.com/helm/chart-releaser/releases/downlo
 tar -xzvf $tmp/cr.tar.gz -C $tmp
 rm -f $tmp/cr.tar.gz
 
+echo "owner 		: $owner"
+echo "git_repo 		: $git_repo"
+echo "git_base_url 	: $git_base_url"
+echo "git_upload_url: $git_upload_url"
+
 $tmp/cr package chart
 $tmp/cr upload -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url -c "$(git rev-parse HEAD)" --skip-existing
 $tmp/cr index -o $owner -r $git_repo -t $token -b $git_base_url -u $ git_upload_url -c "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" --push
